5ecc8d219083840a7427b947d0555405af64b1f6
zxlee <napoleon.lee0114@gmail.com>
2025-11-12 20:11:17 +0800

    refactor: extract FooterNavigation as a separate component

--- src/components/navigation/footer/index.tsx
+++ src/components/navigation/footer/index.tsx
@@ -1,41 +1,24 @@
-import { useLocale } from "next-intl";
+import Logo from "@/components/navigation/Logo";
+import { Wrapper } from "@/components/Wrapper";
 
-import { FOOTER_NAVIGATION } from "@/constants/navigation";
-import { Link } from "@/i18n/navigation";
-
-type Locale = keyof typeof FOOTER_NAVIGATION;
+import FooterNavigation from "./FooterNavigation";
+import SocialLinks from "./SocialLinks";
 
 const Footer = () => {
-  const locale = useLocale() as Locale;
-  const sections = FOOTER_NAVIGATION[locale];
-
   return (
-    <footer className="@container">
-      <div className="flex flex-col gap-5 max-w-7xl mx-auto pt-[60px] pb-20">
-        <nav className="flex-between">
-          <section>
-            <h1>拾光堂</h1>
-            <p>社群連結</p>
+    <footer className="@container/footer">
+      <Wrapper className="flex flex-col gap-10 pt-[60px] pb-20">
+        <nav className="flex-between items-start flex-wrap">
+          <section className="flex flex-col gap-8">
+            <Logo />
+            <SocialLinks />
           </section>
           <section className="flex gap-6">
-            {sections.map(section => (
-              <div key={section.title} className="w-[196px]">
-                <h4 className="text-l-bold mb-2">{section.title}</h4>
-                <ul className="flex flex-col gap-2">
-                  {section.items.map(item => (
-                    <li key={item.destination}>
-                      <Link href={item.link} className="text-m-bold">
-                        {item.destination}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))}
+            <FooterNavigation />
           </section>
         </nav>
-        <p>© Copyright 2025 拾光堂</p>
-      </div>
+        <p className="text-s-bold text-gray-400">© Copyright 2025 拾光堂</p>
+      </Wrapper>
     </footer>
   );
 };
